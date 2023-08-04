d24e23b578c717c6ced5d741dc7457c8f71ea395
elonlee <elonlee@163.com>
2023-08-04 20:52:20 +0800

    add config

--- epub.py
+++ epub.py
@@ -2,31 +2,36 @@
 
 from ebooklib import epub
 import os
+import epubconfig as config
 
 index = 13
-index_end = 123
+index_end = 1052
 output_file = f'output/output.epub'
 
 if __name__ == '__main__':
     book = epub.EpubBook()
 
     # add metadata
-    book.set_identifier('sample123456')
-    book.set_title('测试名字')
-    book.set_language('zh-cn')
+    book.set_title(config.title)
+    book.add_author(config.author)
+    book.set_identifier(config.identifier)
+    book.set_language(config.language)
 
-    book.add_author('测试作者')
-    
     # add cover image
-    book.set_cover("image.jpg", open('cover.jpg', 'rb').read())
+    if config.cover != "":
+        book.set_cover("image.jpg", open(config.cover, 'rb').read())
 
     # intro chapter
-    c1 = epub.EpubHtml(title='Introduction', file_name='intro.xhtml', lang='zh')
-    c1.content=u'<html><head></head><body><h1>Introduction</h1><p>Introduction paragraph where i explain what is happening.</p></body></html>'
+    if config.intro != "":
+        c1 = epub.EpubHtml(title='Introduction',
+                           file_name='intro.xhtml', lang='zh')
+        c1.content = f'<html><head></head><body><h1>前言</h1><p>{config.intro}</p></body></html>'
+        book.add_item(c1)
 
     # defube style
     style = '''BODY { text-align: justify;}'''
-    default_css = epub.EpubItem(uid="style_default", file_name="style/default.css", media_type="text/css", content=style)
+    default_css = epub.EpubItem(
+        uid="style_default", file_name="style/default.css", media_type="text/css", content=style)
     book.add_item(default_css)
     # define css style
     style = '''
@@ -63,11 +68,12 @@ nav[epub|type~='toc'] > ol > li > ol > li {
 '''
 
     # add css file
-    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+    nav_css = epub.EpubItem(
+        uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
     book.add_item(nav_css)
 
     # add chapters to the book
-    book.add_item(c1)
+
     chapters = []
     for i in range(index, index_end+1):
         src = f'output/{i}.txt'
@@ -75,25 +81,27 @@ nav[epub|type~='toc'] > ol > li > ol > li {
         with open(src, 'r', encoding='utf-8') as fsrc:
             title = fsrc.readline()
             content = ""
-            for line in fsrc.readlines(): 
+            for line in fsrc.readlines():
                 line = line.strip()
-                if len(line)>0:
+                if len(line) > 0:
                     content += f'<p>{line}</p>'
             print(f'title={title}')
         # chapter
         ch = epub.EpubHtml(title=title, file_name=f'ch_{i}.xhtml')
-        ch.content=f'<h1>{title}</h1><div>{content}</div>'
+        ch.content = f'<h1>{title}</h1><div>{content}</div>'
         ch.add_item(default_css)
 
         book.add_item(ch)
         chapters.append(ch)
-    
+
     # create table of contents
     # - add manual link
     # - add section
     # - add auto created links to chapters
-
-    book.toc = [epub.Link('intro.xhtml', '前言', 'intro')] + chapters
+    book.toc = []
+    if config.intro != "":
+        book.toc += [epub.Link('intro.xhtml', '前言', 'intro')]
+    book.toc += chapters
 
     # add navigation files
     book.add_item(epub.EpubNcx())
@@ -101,16 +109,14 @@ nav[epub|type~='toc'] > ol > li > ol > li {
     nav.add_item(nav_css)
     book.add_item(nav)
 
-
-    
     # create spin, add cover page as first page
-    book.spine = ['cover', 'nav'] + chapters
+    if config.cover != "":
+        book.spine = ['cover', 'nav'] + chapters
+    else:
+        book.spine = ['nav'] + chapters
     # book.spine = ['nav'] + chapters
 
     # create epub file
     if not os.path.exists("output"):
         os.mkdir("output")
     epub.write_epub(output_file, book, {})
-
-
-
---
+++ epubconfig.py
@@ -0,0 +1,6 @@
+title = 'title'
+author = 'author'
+language = 'zh-cn'
+identifier = 'abc123'
+cover = 'cover.jpg'
+intro = """this is intro"""
